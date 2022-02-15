5f76b646cdc672e3dec318190cb632860024e725
HuynhNhatLinh <nhatlinhgymer2002@gmail.com>
2022-02-15 17:16:19 +0700

    first commit

---
+++ JAVA/test.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+ import java.util.Random;
+ import java.util.Arrays;
+
+    public class test {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in
+            );
+            Random r = new Random();
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            
+
