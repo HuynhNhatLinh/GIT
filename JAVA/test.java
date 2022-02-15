50140f07ab6a3e9021032be2ecd326adfdf578f7
HuynhNhatLinh <nhatlinhgymer2002@gmail.com>
2022-02-15 17:59:57 +0700

    da them phan ngoac main

--- JAVA/test.java
+++ JAVA/test.java
@@ -4,15 +4,15 @@
 
     public class test {
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in
-            // sua loi trong ham main                         
-            );
+            Scanner sc = new Scanner(System.in);
             Random r = new Random();
             int n = sc.nextInt();
             int[] a = new int[n];
             for (int i = 0; i < n; i++) {
                 a[i] = sc.nextInt();
             }
+        }
+    }
             // bo sung them chuc nang
          // bing chilling
 
